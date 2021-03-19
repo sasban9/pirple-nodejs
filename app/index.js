f415df57e2572fe7f8bfeedd26137aa1c530c5aa
sasban <azure.saswata@gmail.com>
2021-03-20 03:53:55 +0530

    Parsing Request Paths

--- app/index.js
+++ app/index.js
@@ -9,7 +9,19 @@ var url = require('url');
 
 // The server should respond to all requests with a string
 var server = http.createServer(function(req,res){
+
+    // Get the URL and parse it
+    var parsedUrl = url.parse(req.url,true);
+
+    // Get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+    //Send the response
     res.end('Hellow World!');
+
+    // Log the request path
+    console.log('Request received on path: '+trimmedPath);
 })
 
 // Start a server, and have it listen on port 3000

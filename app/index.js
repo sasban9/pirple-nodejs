4d7f72483f647b2b8972e5c596e99374a064cfdc
sasban <azure.saswata@gmail.com>
2021-03-20 04:03:18 +0530

    Parsing Query Strings

--- app/index.js
+++ app/index.js
@@ -17,6 +17,9 @@ var server = http.createServer(function(req,res){
     var path = parsedUrl.pathname;
     var trimmedPath = path.replace(/^\/+|\/+$/g,'');
 
+    // Get the query string as an object
+    var queryStringObject = parsedUrl.query;
+
     // Get the HTTP method
     var method = req.method.toLocaleLowerCase();
 
@@ -24,7 +27,7 @@ var server = http.createServer(function(req,res){
     res.end('Hellow World!');
 
     // Log the request path
-    console.log('Request received on path: '+trimmedPath+' with method: '+method);
+    console.log('Request received on path: '+trimmedPath+' with method: '+method+' and these query string parameters ',queryStringObject);
 })
 
 // Start a server, and have it listen on port 3000
